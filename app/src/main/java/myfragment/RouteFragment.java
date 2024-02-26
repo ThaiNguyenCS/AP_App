51bea37cb5b9a28608343581f01546953feef725
Thai Nguyen <nh0kcrazy04@gmail.com>
2024-02-26 21:06:09 +0700

    Add three fragments

---
+++ app/src/main/java/myfragment/RouteFragment.java
@@ -0,0 +1,29 @@
+package myfragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.transportmanagement.R;
+
+public class RouteFragment extends Fragment {
+
+    public RouteFragment() {
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_route, container, false);
+    }
+}
---
+++ app/src/main/java/myfragment/VehicleFragment.java
@@ -0,0 +1,29 @@
+package myfragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.transportmanagement.R;
+
+public class VehicleFragment extends Fragment {
+    public VehicleFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vehicle, container, false);
+    }
+}
