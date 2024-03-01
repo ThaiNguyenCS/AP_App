7bae4e1691e9d40712cbe540204aa06a7f5eb58c
Thai Nguyen <nh0kcrazy04@gmail.com>
2024-03-01 19:54:28 +0700

    Add three detail activity (implement later)

---
+++ app/src/main/java/com/example/transportmanagement/RouteDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.transportmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RouteDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_detail);
+    }
+}
