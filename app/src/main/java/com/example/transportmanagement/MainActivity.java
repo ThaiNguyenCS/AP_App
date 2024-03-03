9789d44096a85a821955c6c2ede12b2c200c4ed3
Thai Nguyen <nh0kcrazy04@gmail.com>
2024-03-03 12:21:44 +0700

    Update

--- app/src/main/java/com/example/transportmanagement/MainActivity.java
+++ app/src/main/java/com/example/transportmanagement/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.transportmanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.transportmanagement.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
+import myfragment.DriverFragment;
+import myfragment.RouteFragment;
 import myfragment.VehicleFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,8 +22,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
-        ////// TEST ////////
-        switchToFragment(new VehicleFragment());
+        mBinding.bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.driver_menuitem)
+                {
+                    switchToFragment(new DriverFragment());
+                    return true;
+                }
+                else if(item.getItemId() == R.id.route_menuitem)
+                {
+                    switchToFragment(new RouteFragment());
+                    return true;
+                }
+                else
+                {
+                    switchToFragment(new VehicleFragment());
+                    return true;
+                }
+            }
+        });
     }
 
     private void switchToFragment(Fragment fragment)
