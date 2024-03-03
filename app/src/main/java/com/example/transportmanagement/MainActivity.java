601584def95c72f13f8785e455f9189524cdd5ff
Thai Nguyen <nh0kcrazy04@gmail.com>
2024-03-03 10:18:03 +0700

    Implement activity main (not done yet)

--- app/src/main/java/com/example/transportmanagement/MainActivity.java
+++ app/src/main/java/com/example/transportmanagement/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.transportmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.example.transportmanagement.databinding.ActivityMainBinding;
+
+import myfragment.VehicleFragment;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+        ////// TEST ////////
+        switchToFragment(new VehicleFragment());
+    }
+
+    private void switchToFragment(Fragment fragment)
+    {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(mBinding.fragmentContainer.getId(), fragment, null)
+                .commit();
     }
 }
--- app/src/main/java/myfragment/VehicleFragment.java
+++ app/src/main/java/myfragment/VehicleFragment.java
@@ -2,6 +2,8 @@ package myfragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,8 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.transportmanagement.R;
+import com.example.transportmanagement.databinding.FragmentVehicleBinding;
 
 public class VehicleFragment extends Fragment {
+    FragmentVehicleBinding mBinding;
     public VehicleFragment() {
         // Required empty public constructor
     }
@@ -24,6 +28,14 @@ public class VehicleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vehicle, container, false);
+        mBinding = FragmentVehicleBinding.inflate(inflater, container, false);
+
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 }
