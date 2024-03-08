3f9fa44459884b656a19498b23f7d92296a283a3
Thai Nguyen <nh0kcrazy04@gmail.com>
2024-03-08 10:34:38 +0700

    new feature

---
+++ app/src/main/java/com/example/transportmanagement/AssignJobActivity.java
@@ -0,0 +1,14 @@
+package com.example.transportmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AssignJobActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assign_job);
+    }
+}
